28fc6ba9fc339212a0ecbdbd40c72279a3e3a14d
themonster2015 <quynhyen.vothi@gmail.com>
2017-02-27 20:12:27 +0100

    2.10

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 // Twitter
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
+function initMap() {
+        var uluru = {lat: 41.646406, lng: -0.883709}; 
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+
 
 $(document).ready(function(){
   // Smooth scrolling
