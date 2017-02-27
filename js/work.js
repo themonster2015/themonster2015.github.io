234dc2cfbc76912079548924ba762bb8d4c82472
themonster2015 <quynhyen.vothi@gmail.com>
2017-02-27 04:05:16 +0100

    2.9

--- js/work.js
+++ js/work.js
@@ -1,22 +1,49 @@
 var works = [
-  "img/1.jpeg",
-  "img/1.jpeg",
-  "img/1.jpeg",
-  "img/1.jpeg",
-  "img/1.jpeg",
-  "img/1.jpeg",
-  "img/1.jpeg",
-  "img/1.jpeg"
+    {
+    title: "First Project",
+    pic: "img/1.jpeg"
+  },
+  {
+    title: "Second Project",
+    pic: "img/1.jpeg"
+  },
+  {
+    title: "Third Project",
+    pic: "img/1.jpeg"
+  },
+  {
+    title: "Fourth Project",
+    pic: "img/1.jpeg"
+  }
+ //,
+  // {
+  //   title: "Fifth Project",
+  //   pic: "img/1.jpeg"
+  // },
+  // {
+  //   title: "Sixth Project",
+  //   pic: "img/1.jpeg"
+  // },
+  // {
+  //   title: "Seventh Project",
+  //   pic: "img/1.jpeg"
+  // },
+  // {
+  //   title: "Eighth Project",
+  //   pic: "img/1.jpeg"
+  // }
 ]
 
 // work section
   console.log(works);
  for(var i = 0; i < works.length; ++i ) {
   $("#work").append("\
-    <div class='col-sm-6 col-md-3'>\
-      <img class='img-responsive' src='" + works[i] + "'>\
-    </div>\
-  ");
+  <div class='col-sm-3 col-md-3'>\
+    <a href='#work' class='work-img' ><img class='img-responsive' src='" + works[i].pic + "'>\
+  <span class='info'><p class='proj-title'>Title:</p>" + works[i].title  + "</span></a>\
+    </a>\
+  </div>\
+");
   var images = $("#work img");
   if(i%2 === 0){
     $(images[i]).css("border", "2px solid DodgerBlue");
@@ -24,3 +51,9 @@ var works = [
     $(images[i]).css("border", "2px solid salmon");
   };
 };
+
+$(".work-img").mouseenter(function(){
+  $(".info", this).show();
+}).mouseleave(function(){
+  $(".info", this).hide();
+});
